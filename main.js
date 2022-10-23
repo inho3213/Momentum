cc8090b3b476e24ce934d2c84042305713007d77
inho3213 <61544239+inho3213@users.noreply.github.com>
2022-10-23 22:29:06 +0900

    html form

---
+++ main.js
@@ -0,0 +1,12 @@
+const loginInput = document.querySelector('#login-form input');
+const loginButton = document.querySelector('#login-form button');
+
+loginButton.addEventListener('click', ()=>{
+    const username = loginInput.value;
+    // if(username === ""){
+    //     alert('please write your name');
+    // } else if(username.length > 15) {
+    //     alert('Your name is too long!');
+    // }
+    
+});
