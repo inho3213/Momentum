e46be5b3c1a98e091eb644c1b024b28177f4a415
inho3213 <61544239+inho3213@users.noreply.github.com>
2022-10-25 22:45:33 +0900

    js quotes array
    나중에 더 추가해야함

---
+++ js/quotes.js
@@ -0,0 +1,39 @@
+const quotes = [
+    {
+        quote:"\"If you can dream it, you can do it\"",
+        author:"Walt Disney",
+    },
+    {
+        quote:"\"It always seems impossible until it's done\"",
+        author:"Nelson Mandela",
+    },
+    {
+        quote:"\"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time\"",
+        author:"Thomas A. Edison",
+    },
+    {
+        quote:"\"If you quit once it becomes a habit. Never quit\"",
+        author:"Michael Jordan",
+    },
+    {
+        quote:"\"Simplicity is the ultimate sophistication\"",
+        author:"Leonardo da Vinci",
+    },
+    {
+        quote:"\"The secret of happiness is renunciation\"",
+        author:"Andrew Carnegie",
+    },
+    {
+        quote:"\"젊음의 매 순간이 기회임을 젊음은 종종 잊는다\"",
+        author:"Unknown",
+    },
+    {
+        quote:"\"계획을 세워도 실천하지 않으면 아무것도 시작되지 않는다\"",
+        author:"Shohei Ohtani",
+    },
+    {
+        quote:"\"Obstacles are those frightful things you see when you take your eyes off your goal\"",
+        author:"Henry Ford",
+    },
+]
+
