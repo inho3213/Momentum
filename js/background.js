dbfde350fb9adf04875dbcb8549a3011ad39afdc
inho3213 <61544239+inho3213@users.noreply.github.com>
2022-11-04 21:28:04 +0900

    CSS background, clock, input

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 const images =["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg" ];
 const randomImage = images[Math.floor(Math.random()*images.length)]; 
 
-const backgroundImage = document.createElement("img");
-backgroundImage.src = `img/${randomImage}`;
+const bgImage = document.createElement("img");
+const imageUrl = `img/${randomImage}`;
+bgImage.src = imageUrl;
+// console.log(bgImage);
+document.body.style.backgroundImage = `url(${imageUrl})`;
 
-document.body.appendChild(backgroundImage)    //body 부분에 추가하기
+const form = document.querySelector('#login-form')
