c9f53b52f44af5ea66c3a9392bb8b75db4cbc98a
inho3213 <61544239+inho3213@users.noreply.github.com>
2022-11-21 22:48:04 +0900

    background

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const loginForm = document.querySelector('#login-form');
-const loginInput = document.querySelector('#login-form input');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
 // loginButton.addEventListener('click', ()=>{
 //     const username = loginInput.value;
@@ -8,24 +8,24 @@ const loginInput = document.querySelector('#login-form input');
 //     // } else if(username.length > 15) {
 //     //     alert('Your name is too long!');
 //     // }
-    
+
 // });
 
-const greeting = document.querySelector('#greeting');
+const greeting = document.querySelector("#greeting");
 
-loginForm.addEventListener('submit', (event)=>{
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    event.preventDefault(); //submit 했을때 자동으로 새로고침 하는거 막으려고 넣음
-    loginForm.classList.add('hidden');
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove('hidden');
-})
+loginForm.addEventListener("submit", (event) => {
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  event.preventDefault(); //submit 했을때 자동으로 새로고침 하는거 막으려고 넣음
+  loginForm.classList.add("hidden");
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove("hidden");
+});
 
-const savedUsername = localStorage.getItem('username');
-if(savedUsername === null){
-    loginForm.classList.remove('hidden');
-}else{
-    greeting.innerText = `Hello ${savedUsername}`;
-    greeting.classList.remove('hidden');
+const savedUsername = localStorage.getItem("username");
+if (savedUsername === null) {
+  loginForm.classList.remove("hidden");
+} else {
+  greeting.innerText = `Hello, ${savedUsername}`;
+  greeting.classList.remove("hidden");
 }
